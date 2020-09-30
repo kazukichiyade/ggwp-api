959118a22432dadbc0bfe198b447b9b1bd572005
kazukichi <zexyl_x@icloud.com>
2020-10-01 00:08:31 +0900

    ユーザー作成関数作成

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionMixin,
+)
+from django.conf import settings
 
-# Create your models here.
+
+# BaseUserManagerをオーバーライド(username認証からemail認証に変更)
+class UserManager(BaseUserManager):
+
+    # ユーザー作成関数
+    def create_user(self, email, password=None):
+
+        # emailが無い場合、raiseで例外を発生させる
+        if not email:
+            raise ValueError("email is must")
+
+        # インスタンス作成及びemail格納(取得したemailが大文字も小文字になるように設定)
+        user = self.model(email=self.normalize_email(email))
+
+        # 引数で取得したパスワードをハッシュ化
+        user.set_password(password)
+
+        # DB保存
+        user.save(using=self._db)
+
+        return user
